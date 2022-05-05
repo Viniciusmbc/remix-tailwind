ae0ef67829769cb00cf5b614466bb71b60e78b0a
Viniciusmbc <viniciuscaceres@gmail.com>
2022-05-05 19:35:07 -0300

    adicionado searchbar

--- app/components/Countries.tsx
+++ app/components/Countries.tsx
@@ -5,55 +5,139 @@ export default function Countries({ data }) {
   const [query, setQuery] = useState("");
 
   return (
-<section className="max-w-screen-lg mx-auto">
-<div className="flex border-2 rounded">
-        <div className="flex items-center justify-center px-4 border-r">
-            <svg className="w-6 h-6 bg-white" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24">
-                <path
-                    d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z">
-                </path>
-            </svg>
-        </div>
-        <input type="text" className="px-4 py-2 w-80" placeholder="Search..."/>
-    </div>
-  
-            <article  className="grid grid-cols-4 mx-auto">
-            {data.filter(({ name }) => {
-          if (name === "") {
-            return data;
-          } else if (name.common.toLowerCase().includes(query.toLowerCase())) {
-            return data;
-          }
-        }).map((country) => (
-          <div
-            key={country.name.common}
-            className="mx-4 bg-slate-300 rounded-md"
+    <section className="max-w-screen-lg mx-auto">
+      <form>
+        <div className="flex items-center bg-white  border-2 mx-4 border-cyan-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 mr-2 text-gray-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            <img
-              className=" h-44 rounded-t-md shadow-lg shadow-slate-300" 
-              src={country.flags.png}
-              alt={country.name.common}
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
+          </svg>
+          <input
+            type="text"
+            name="name"
+            placeholder="Search for a country..."
+            className="w-1/3 py-2  outline-none"
+          />
+        </div>
+        <button
+          id="dropdownDefault"
+          data-dropdown-toggle="dropdown"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          type="button"
+        >
+          Dropdown button{" "}
+          <svg
+            className="w-4 h-4 ml-2"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </button>
 
-            <Link to={`/country/${country.cca3.toLowerCase()}`}>
-              <h2 className="text-2xl font-bold py-1 pl-3">{country.name.common}</h2>
-            </Link>
+        <div
+          id="dropdown"
+          className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700"
+        >
+          <ul
+            className="py-1 text-sm text-gray-700 dark:text-gray-200"
+            aria-labelledby="dropdownDefault"
+          >
+            <li>
+              <div
+                href="#"
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Dashboard
+              </div>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Settings
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Earnings
+              </a>
+            </li>
+            <li>
+              <div
+                href="#"
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Sign out
+              </div>
+            </li>
+          </ul>
+        </div>
+      </form>
 
-            <p className=" pl-3 py-1">
-              {" "}
-              <b>Population:</b> {country.population}
-            </p>
-            <p className=" pl-3 py-1">
-              <b>Region: </b> {country.region}
-            </p>
-            <p className=" pl-3 py-1">
-              <b>Capital: </b> {country.capital}
-            </p>
-          </div>
-        ))
-        .splice(0, 8)}
-            </article>
-</section>
+      <article className="grid grid-cols-4 mx-auto">
+        {data
+          .filter(({ name }) => {
+            if (name === "") {
+              return data;
+            } else if (
+              name.common.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return data;
+            }
+          })
+          .map((country) => (
+            <div
+              key={country.name.common}
+              className="mx-4 bg-slate-300 rounded-md"
+            >
+              <img
+                className=" h-44 rounded-t-md shadow-lg shadow-slate-300"
+                src={country.flags.png}
+                alt={country.name.common}
+              />
+
+              <Link to={`/country/${country.cca3.toLowerCase()}`}>
+                <h2 className="text-2xl font-bold py-1 pl-3">
+                  {country.name.common}
+                </h2>
+              </Link>
+
+              <p className=" pl-3 py-1">
+                {" "}
+                <b>Population:</b> {country.population}
+              </p>
+              <p className=" pl-3 py-1">
+                <b>Region: </b> {country.region}
+              </p>
+              <p className=" pl-3 py-1">
+                <b>Capital: </b> {country.capital}
+              </p>
+            </div>
+          ))
+          .splice(0, 8)}
+      </article>
+    </section>
   );
 }
---
+++ remix-tailwind-boilerplate/app/routes/index.jsx
@@ -0,0 +1,24 @@
+import { Link, useLoaderData, json, Outlet } from "remix";
+import Countries from "~/components/Countries";
+import Navbar from "~/components/Navbar";
+
+export async function loader() {
+  const response = await fetch(
+    "https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital,cca3"
+  );
+
+  const countries = json(await response.json());
+
+  return countries;
+}
+
+
+export default function Index() {
+  const countries = useLoaderData();
+
+  return (
+    <main className=" bg-slate-500 min-h-screen ">
+    <Countries data={countries}/>
+    </main>
+  );
+}
